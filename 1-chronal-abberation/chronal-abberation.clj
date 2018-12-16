ebf005e96fda500ab7ce20076b0791d80c2d4aba
Snorre Magnus Davøen <post@snorre.io>
2018-12-16 23:04:21 +0100

    Clojure solution to day 1 of advent of code calendar
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ 1-chronal-abberation/chronal-abberation.clj
@@ -0,0 +1,33 @@
+(ns chronal_abberation
+  (:require [clojure.string :refer [split]]))
+
+
+(defn sum-of-frequencies
+  []
+  (as-> (slurp "./1-chronal-abberation/input.txt") s
+        (split s #"\n")
+        (map read-string s)
+        (apply + s)))
+
+
+(defn find-repeated
+  []
+  (as-> (slurp "./1-chronal-abberation/input.txt") s
+        (split s #"\n")
+        (map read-string s)
+        (repeat s)
+        (flatten s)
+        (reduce (fn [acc freq]
+                  (if ((:seen acc) (:freq acc))
+                    (reduced (assoc acc :res (:freq acc)))
+                    (-> acc
+                        (update :seen #(conj % (:freq acc)))
+                        (update :freq #(+ % freq)))))
+                {:freq 0 :seen #{} :res nil}
+                s)
+        (:res s)))
+
+
+(comment
+  (sum-of-frequencies)
+  (find-repeated))
